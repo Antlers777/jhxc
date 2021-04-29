d6efa196da0d3a4e41776eca1694b27e1223f3f6
赵晓桐 <919994844@qq.com>
2021-04-29 17:01:12 +0800

    标注功能改进

---
+++ XunCha/Model/MarkMenu.swift
@@ -0,0 +1,20 @@
+//
+//  MarkMenu.swift
+//  XunCha
+//
+//  Created by 赵晓桐 on 2021/4/29.
+//
+
+import Foundation
+
+public enum MarkMenu {
+    case normal
+    case x
+//    case mountain
+//    case signallamp
+//    case river
+//    case house
+//    case wc
+    case text
+        
+}
